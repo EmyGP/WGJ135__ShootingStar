af04570070d2d06f04b840f546ea795270bfd1bc
EMYGP\EmyGP <emyburgp@gmail.com>
2020-02-13 15:57:40 +0100

    aggiunto un behavior per i muri

---
+++ Assets/My_STUFF/Scripts/BorderBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "asteroid")
+        {
+            //other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+            //distruggi asteroide
+            //animazioni bellissime
+            //RIDURRE lo score
+            //spawna altri N ateroidi???
+        }
+    }
+}
