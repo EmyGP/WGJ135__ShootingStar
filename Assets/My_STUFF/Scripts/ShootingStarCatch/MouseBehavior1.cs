e223d7077a9af865760b1aac9f063b81109aafb5
EMYGP\EmyGP <emyburgp@gmail.com>
2020-02-14 00:16:37 +0100

    cambio di programma; aggiunti vari shooter point al posto degli spawner; creazione del timer degli shooter

---
+++ Assets/My_STUFF/Scripts/ShootingStarCatch/MouseBehavior1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseBehavior1 : MonoBehaviour
+{
+    public ScoreCounter scoreCounter;
+    //public GameObject[] starPrefab;
+    public GameObject starPrefabObj;
+    private Collider2D starCollider;
+
+    private void Start()
+    {
+        //foreach (GameObject starP in starPrefab)
+        //{
+                ////per ogni gameobject nell'array starPrefab definisco la mia variabile starcollider come il collider2D di starP
+        //    starCollider = starP.GetComponent<CircleCollider2D>();
+        //}
+        starCollider = starPrefabObj.GetComponent<CircleCollider2D>();
+    }
+
+
+    private void OnMouseDown()
+    {
+        scoreCounter.ScoreFunction();
+        starCollider.enabled = false;
+    }
+}
---
+++ Assets/My_STUFF/Scripts/ShootingStarCatch/Spawner1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner1 : MonoBehaviour
+{
+    public Transform[] spawnPoint;
+    public GameObject asteroid;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            foreach (Transform sP in spawnPoint)
+            {
+                //uso il foreach così posso spararne anche più di uno nello stesso momento
+                Instantiate(asteroid, sP.position, sP.rotation);
+                //aggiungere ciò che le sparia per aria
+            }
+        }
+
+        
+    }
+}
