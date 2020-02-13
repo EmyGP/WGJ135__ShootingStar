6eb68920cfba3de2841c5fec46b119f5c5e8f98b
EMYGP\EmyGP <emyburgp@gmail.com>
2020-02-13 14:04:55 +0100

    aggiunto very-proto asteroide

--- Assets/My_STUFF/Scripts/PBall_behavior.cs
+++ Assets/My_STUFF/Scripts/PBall_behavior.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class PBall_behavior : MonoBehaviour
 {
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "asteroid")
+        if (other.tag == "asteroid")
         {
             other.gameObject.SetActive(false);
-            Debug.Log("colpitoooo!");
             //distruggi asteroide
+            //animazioni bellissime
             //aumenta lo score
             //spawna altri N ateroidi???
         }
