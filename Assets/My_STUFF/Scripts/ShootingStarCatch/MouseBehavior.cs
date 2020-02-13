7a2b4fd32ca6106710f0adc5e6e4c391370977aa
EMYGP\EmyGP <emyburgp@gmail.com>
2020-02-13 17:15:09 +0100

    aggiunto score counter, aggiunte stelle + mouse controller

---
+++ Assets/My_STUFF/Scripts/ShootingStarCatch/MouseBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseBehavior : MonoBehaviour
+{
+    public ScoreCounter scoreCounter;
+
+    private void OnMouseDown()
+    {
+        scoreCounter.ScoreFunction();
+    }
+}
---
+++ Assets/My_STUFF/Scripts/ShootingStarCatch/ScoreCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    public void ScoreFunction()
+    {
+        score = score + 1;
+        scoreText.text = " " + Mathf.Round(score);
+        //scivi sul text il punteggio nuovo
+        Debug.Log("+1");
+    }
+}
