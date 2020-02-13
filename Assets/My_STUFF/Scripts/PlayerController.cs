99ecc55e66bd0b5fd54754d27c9b980cf748da00
EMYGP\EmyGP <emyburgp@gmail.com>
2020-02-13 09:23:32 +0100

    aggiunto player mov controller

---
+++ Assets/My_STUFF/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        //il player si muove seguendo la posizione del mouse sullo schermo
+        //Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = mousePos;
+        transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,transform.position.y);
+    }
+}
