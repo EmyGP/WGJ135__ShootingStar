59f7a1564d73773459b171f948359af97ad2366d
EMYGP\EmyGP <emyburgp@gmail.com>
2020-02-14 01:21:34 +0100

    test su coroutine con for (mhmh)

--- Assets/My_STUFF/Scripts/ShooterTimer.cs
+++ Assets/My_STUFF/Scripts/ShooterTimer.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ShooterTimer : MonoBehaviour
 {
+    public float timerGame = 60;
+    public float timerShoot_1 = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("TimerShooter");      
     }
 
-    // Update is called once per frame
+    //coroutine che gestisce il timer degli spawn point. Quando il timer arriv a a0 lo spawn deve sparare di nuovo e il timer ripartire
+    IEnumerator TimerShooter()
+    {
+        for(float i = 60; i > timerShoot_1; i--)
+        {
+            timerShoot_1 -= i*timerShoot_1*Time.deltaTime;
+            yield return null;
+        }
+        //if (timerShoot_1 >= 0)
+        //{
+        //    timerShoot_1 -= Time.deltaTime;
+        //    yield return timerShoot_1;
+        //}
+        //else
+        //{
+        //    //shoot altre stelle
+        //    timerShoot_1 = 10;
+        //    yield return null;
+        //}
+    }
+
+    // ci vorrebbe più una coroutine così posso fermarla
     void Update()
     {
-        
+        timerGame -= Time.deltaTime;      
     }
 }
--- Assets/My_STUFF/Scripts/ShootingStarCatch/Spawner1.cs
+++ Assets/My_STUFF/Scripts/ShootingStarCatch/Spawner1.cs
@@ -7,18 +7,21 @@ public class Spawner1 : MonoBehaviour
     public Transform[] spawnPoint;
     public GameObject asteroid;
 
+    public void ShootStars()
+    {
+        foreach (Transform sP in spawnPoint)
+        {
+            //uso il foreach così posso spararne anche più di uno nello stesso momento
+            Instantiate(asteroid, sP.position, sP.rotation);
+            //aggiungere ciò che le sparia per aria
+        }
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            foreach (Transform sP in spawnPoint)
-            {
-                //uso il foreach così posso spararne anche più di uno nello stesso momento
-                Instantiate(asteroid, sP.position, sP.rotation);
-                //aggiungere ciò che le sparia per aria
-            }
-        }
-
-        
+            ShootStars();
+        }        
     }
 }
