50f1e89b90c9bc6625cb92b0b5fb6a7753b14915
EMYGP\EmyGP <emyburgp@gmail.com>
2020-02-16 10:14:27 +0100

    aggiunto shooter per stelle (ancora da fixare il timer spawn)

--- Assets/My_STUFF/Scripts/ShootingStarCatch/Spawner1.cs
+++ Assets/My_STUFF/Scripts/ShootingStarCatch/Spawner1.cs
@@ -6,14 +6,19 @@ public class Spawner1 : MonoBehaviour
 {
     public Transform[] spawnPoint;
     public GameObject asteroid;
+    public Rigidbody2D asteroidRb;
+    public int shootForce;
 
     public void ShootStars()
     {
+        //uso il foreach così posso spararne anche più di uno nello stesso momento
         foreach (Transform sP in spawnPoint)
         {
-            //uso il foreach così posso spararne anche più di uno nello stesso momento
-            Instantiate(asteroid, sP.position, sP.rotation);
+            //Instantiate(asteroid, sP.position, sP.rotation);
             //aggiungere ciò che le sparia per aria
+            Rigidbody2D starInstance;
+            starInstance = Instantiate(asteroidRb, sP.position, sP.rotation) as Rigidbody2D;
+            starInstance.AddForce(sP.up * shootForce);
         }
     }
 
